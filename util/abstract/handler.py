99a250bedca7bd164c5b767922218a9497498918
Antosik <grig_anton96@mail.ru>
2020-11-24 20:36:03 +0300

    feat: async processing

--- util/abstract/handler.py
+++ util/abstract/handler.py
@@ -1,4 +1,8 @@
-from typing import Any, Dict, Final, List
+from typing import Any, Dict, Final, List, Optional, Tuple
+
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from os import environ as env
 
 from util.abstract.feed import Feed
 from util.functions.normalize_url import normalize_url
@@ -11,6 +15,9 @@ class Handler(object):
 
     TARGET_DIR: Final = '/tmp/'
     VERSION_DIR: Final = '/v3/'
+
+    MAX_POOL_STR: Final = env.get('MAX_POOL')
+    MAX_POOL: Final = (MAX_POOL_STR.isnumeric() and int(MAX_POOL_STR)) or 10
 
     def __init__(self):
         """Constructor"""
@@ -57,23 +64,56 @@ class Handler(object):
         """
         raise NotImplementedError
 
+    def __process_server_task(self, server: Dict[str, Any]) -> Optional[Tuple[str, str]]:
+        """Internal method - process server and return filepaths
+
+        Args:
+            server (Dict[str, Any]): Server data
+
+        Returns:
+            Optional[Tuple[str, str]]: Fullpath to file and filename for S3
+        """
+
+        filepath = normalize_url(self.VERSION_DIR + self.get_filepath(server))
+        fullpath = normalize_url(self.TARGET_DIR + filepath)
+
+        selfLink = S3.generate_link(filepath)
+        feed = self.process_server(server)
+        feed.generateUUID(selfLink)
+        feed.setSelfLink(selfLink)
+
+        old_feed = self.__parser.parse(selfLink)
+
+        if old_feed != feed:
+            self.__generator.generate(feed, fullpath)
+            return (fullpath, filepath[1:])
+
+        return None
+
+    async def __process_servers(self, servers: List[Dict[str, Any]]):
+        """Internal method - Async processing of servers
+
+        Args:
+            servers (List[Dict[str, Any]]): List of servers
+        """
+
+        loop = asyncio.get_running_loop()
+        executor = ThreadPoolExecutor(max_workers=self.MAX_POOL)
+
+        paths = await asyncio.gather(
+            *[
+                loop.run_in_executor(executor, self.__process_server_task, server) for server in servers
+            ]
+        )
+
+        for p in paths:
+            if p:
+                fullpath, filepath = p
+                self.__s3.upload(fullpath, filepath)
+
     def run(self):
         """Run a feed generation process for all service-related servers"""
 
         servers = self.load_servers()
-
-        for server in servers:
-
-            filepath = normalize_url(self.VERSION_DIR + self.get_filepath(server))
-            fullpath = normalize_url(self.TARGET_DIR + filepath)
-
-            selfLink = S3.generate_link(filepath)
-            feed = self.process_server(server)
-            feed.generateUUID(selfLink)
-            feed.setSelfLink(selfLink)
-
-            old_feed = self.__parser.parse(selfLink)
-
-            if old_feed != feed:
-                self.__generator.generate(feed, fullpath)
-                self.__s3.upload(fullpath, filepath[1:])
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.__process_servers(servers))
